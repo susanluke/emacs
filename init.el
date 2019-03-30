9a8dc1c1612cd1b16c4b702a16e59023b18f2881
Susan Luke <susan.luke@fundingcircle.com>
2019-03-30 17:55:48 +0000

    switch to helm a bit

--- init.el
+++ init.el
@@ -51,7 +51,6 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-
 (setq inhibit-startup-message t)
 (global-linum-mode t)
 
@@ -59,7 +58,7 @@
 (require 'helm-projectile)
 
 (helm-mode t)
-
+(helm-projectile-on)
 
 (tool-bar-mode -1)
 (add-to-list 'exec-path "/usr/local/bin/")
@@ -155,7 +154,7 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'highlight-symbol-mode)
 (add-hook 'clojure-mode-hook 'company-mode)
-(add-hook 'clojure-mode-hook 'linum-mode)
+;;(add-hook 'clojure-mode-hook 'linum-mode)
 (add-hook 'clojure-mode-hook 'show-paren-mode)
 (add-hook 'clojure-mode-hook 'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook 'fci-mode)
@@ -194,7 +193,8 @@
 
 ;; Projectile setup
 (projectile-mode)
-(global-set-key (kbd "C-.") 'counsel-projectile-ag)
+;;(global-set-key (kbd "C-.") 'counsel-projectile-ag)
+(global-set-key (kbd "C-.") 'helm-projectile-ag)
 
 ;; Multiple Cursors setup
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
@@ -202,7 +202,7 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this-symbol)
 (global-set-key (kbd "C-c <") 'mc/mark-all-symbols-like-this-in-defun)
 
-;; Browse kill ring aetup
+;; Browse kill ring setup
 (global-set-key (kbd "C-c k") 'browse-kill-ring)
 
 ;; Company mode setup
@@ -225,7 +225,7 @@
     ("e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "e3fc83cdb5f9db0d0df205f5da89af76feda8c56d79a653a5d092c82c7447e02" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(package-selected-packages
    (quote
-    (markdown-mode feature-mode git-gutter-fringe git-gutter+ pretty-symbols elein clj-refactor highlight-indentation flycheck-joker zenburn-theme json-mode dracula-theme spacemacs-theme smart-mode-line leuven-theme helm-themes intellij-theme fill-column-indicator yaml-mode neotree solarized-theme browse-kill-ring exec-path-from-shell multiple-cursors counsel-projectile projectile ivy-hydra company counsel swiper ivy expand-region highlight-symbol undo-tree paredit magit cider)))
+    (aggressive-indent markdown-mode feature-mode git-gutter-fringe git-gutter+ pretty-symbols elein clj-refactor highlight-indentation flycheck-joker zenburn-theme json-mode dracula-theme spacemacs-theme smart-mode-line leuven-theme helm-themes intellij-theme fill-column-indicator yaml-mode neotree solarized-theme browse-kill-ring exec-path-from-shell multiple-cursors counsel-projectile projectile ivy-hydra company counsel swiper ivy expand-region highlight-symbol undo-tree paredit magit cider)))
  '(python-shell-interpreter "python3")
  '(safe-local-variable-values
    (quote
@@ -284,20 +284,21 @@
 ;; from - https://emacs.stackexchange.com/questions/37678/neotree-window-not-resizable
 (setq neo-window-fixed-size nil)
 ;; NeoTree to work with Projectile - from https://www.emacswiki.org/emacs/NeoTree
-(setq projectile-switch-project-action 'neotree-projectile-action)
+;;(setq projectile-switch-project-action 'neotree-projectile-action)
 (global-set-key [f8] 'neotree-project-dir)
-(defun neotree-project-dir ()
-  "Open NeoTree using the git root."
-  (interactive)
-  (let ((project-dir (projectile-project-root))
-        (file-name (buffer-file-name)))
-    (neotree-toggle)
-    (if project-dir
-        (if (neo-global--window-exists-p)
-            (progn
-              (neotree-dir project-dir)
-              (neotree-find file-name)))
-      (message "Could not find git project root."))))
+;;;; Commented out as it doesn't work
+;; (defun neotree-project-dir ()
+;;   "Open NeoTree using the git root."
+;;   (interactive)
+;;   (let ((project-dir (projectile-project-root))
+;;         (file-name (buffer-file-name)))
+;;     (neotree-toggle)
+;;     (if project-dir
+;;         (if (neo-global--window-exists-p)
+;;             (progn
+;;               (neotree-dir project-dir)
+;;               (neotree-find file-name)))
+;;       (message "Could not find git project root."))))
 
 ;; Some code from Andrea to be able to evaluate code in org mode
 (org-babel-do-load-languages
